1d3c7be4bb76e7ca8d5787fbf97e57a274848961
Aleksei Makarov <makarovalexeynz@gmail.com>
2024-03-07 00:03:34 +0300

    первое исправление замечаний ревьюера

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -3,83 +3,84 @@
 #include <cmath>
 #include "calculator.h"
 
-bool ReadNumber(Number& result){
-    std::cin>>result;
-    if(std::cin.fail()){
-        std::cerr<<"Error: Numeric operand expected"<<std::endl;
+bool ReadNumber(Number& result) {
+    std::cin >> result;
+    if (std::cin.fail()) {
+        std::cerr << "Error: Numeric operand expected" << std::endl;
         return false;    
     }
-    else{
+    else {
         return true;     
     }
 }
 
 bool RunCalculatorCycle() {
-    Number savevar; // сохраненное значение, значение над которым производят операции
-    Number var;  // значение после оператора
-    std::string op; // оператор
-    Number memory; // ячейка памяти для сохранения 
-    bool open=true; // true в случае если ячейка паняти свободна
+    Number save_var = 0; // сохраненное значение, значение над которым производят операции
+    Number var = 0;  // значение после оператора
+    Number memory = 0; // ячейка памяти для сохранения 
+    bool open = true; // true в случае если ячейка памяти свободна
     bool last = false;  // для возврата всей функции, только в случае ввода q возвращает true
     
-    if (ReadNumber(savevar)){
-        while(std::cin>>op){ 
-            if (op=="q"){
-                last=true; break;
+    if (ReadNumber(save_var)) {
+        std::string op; // оператор
+        while (std::cin >> op) { 
+            if (op == "q") {
+                return true;
             }
-            else if (op=="=") {
-                std::cout<<savevar<<std::endl;
+            else if (op == "=") {
+                std::cout << save_var << std::endl;
             }
-            else if (op=="+") {
-                if(ReadNumber(var)) {
-                    savevar += var;
+            else if (op == "+") {
+                if (ReadNumber(var)) {
+                    save_var += var;
                 } 
                 else break;
             }
-            else if (op=="-") {
-                if(ReadNumber(var)) {
-                    savevar -= var;
+            else if (op == "-") {
+                if (ReadNumber(var)) {
+                    save_var -= var;
                 } 
                 else break;    
             } 
-            else if (op=="*") {
-                if(ReadNumber(var)) {
-                    savevar *= var;} 
+            else if (op == "*") {
+                if (ReadNumber(var)) {
+                    save_var *= var;
+                } 
                 else break;
             } 
-            else if (op=="/") {
-                if(ReadNumber(var)) {
-                    savevar /= var;
+            else if (op == "/") {
+                if (ReadNumber(var)) {
+                    save_var /= var;
                 } 
                 else break;    
             } 
-            else if (op=="**"){
-                if(ReadNumber(var)) {
-                    savevar= std::pow(savevar, var);
+            else if (op == "**") {
+                if (ReadNumber(var)) {
+                    save_var = std::pow(save_var, var);
                 } 
                 else break;    
             } 
-            else if (op ==":") {
-                ReadNumber(savevar); 
+            else if (op == ":") {
+                ReadNumber(save_var); 
             }
             else if (op =="c") {
-                savevar=0; 
+                save_var=0; 
             }
             else if (op=="s") {
-                memory=savevar;
+                memory=save_var;
                 open=false;
             }
             else if (op=="l") {
-                if(open){
-                    std::cerr<<"Error: Memory is empty"<<std::endl; 
+                if (open) {
+                    std::cerr << "Error: Memory is empty" << std::endl; 
                     break;
                 } 
-                else if (!open){
-                    savevar=memory;
+                else if (!open) {
+                    save_var = memory;
                 } 
             }
             else {
-                std::cerr<<"Error: Unknown token "<<op<<std::endl; 
+                std::cerr << "Error: Unknown token " << op <<std::endl; 
                 break;
             }
         }
